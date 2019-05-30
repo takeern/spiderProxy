a3a0291a1e35b64cdba8e87f44d4178b0510a565
takeern <tq12346@foxmail.com>
2019-05-30 11:22:43 +0000

    [update]

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 	"net/url"
 )
 
+type SearchResp struct {
+	BookName	string
+	BookNumber	string
+	BookState	bool
+	BookIntro	string
+}
+
 func searchBook(bookName string) (resHtml string, err error) {
 	url := "https://www.aixdzs.com/bsearch?q=" + url.QueryEscape(bookName)
 	res, err := http.Get(url)
@@ -16,16 +23,27 @@ func searchBook(bookName string) (resHtml string, err error) {
 	}
 	defer res.Body.Close()
 	Body, err := ioutil.ReadAll(res.Body)
-	return Body, err
+	return string(Body), err
+}
+
+func praseHtml(html string) SearchResp {
+	s := SearchResp{
+		BookName: "大道",
+		BookNumber: "ddsds",
+		BookState: true,
+		BookIntro: "dsdsds",
+	}
+	return s
 }
 
 func main() {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 
 	bookName := "大道朝天"
-	Body, err := searchBook(bookName)
+	_, err := searchBook(bookName)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("body html /n s%", Body)
+	s := praseHtml("dsdss")
+	fmt.Println("body html /n s%", s)
 }
